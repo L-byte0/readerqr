dbe62fc2155142859b8f247dde3fa0c1eef408aa
macro-bits <luis852luis@gmail.com>
2022-11-23 21:05:05 -0600

    Final

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,9 +1,11 @@
 <?php
 namespace App\Controllers;
 use App\Models\Visitor;
+use Config\Services;
 
 class Home extends BaseController
 {
+    protected $helpers = ['form'];
 
     public function __construct()
     {
@@ -12,21 +14,55 @@ class Home extends BaseController
 
     public function index()
     {
-        return view('scanner');
+        $data = [
+            'email' => [
+                'type' => 'hidden',
+                'name' => 'email',
+                'class' => 'form-control form-control-sm',
+                'value' => set_value('email'),
+                'placeholder' => 'Correo electrónico',
+                'id' => 'email',
+            ],
+            'validation' => Services::validation(),
+        ];
         
-    }
-    
-
-    public function saveUser(){
-        try {
-            $this->visitor->insert([
-                'email' => $this->request->getPost('text'),
-            ]);
-
-            return redirect()->route('/');
-            
-        } catch (\Exception $e) {
-            session()->setFlashdata('error', $e->getMessage());
+        if (strtolower($this->request->getMethod()) !== 'post') {
+            return view('templates/header') . view('scanner', $data) . view('templates/footer');
         }
+
+        if (!$this->validate([
+            'email' => [
+                'label' => $data['email']['placeholder'],
+                'rules' => 'required|valid_email|is_unique[visitor.email]',
+                'errors' => [
+                    'required' => '{field} un campo obligatorio',
+                    'valid_email' => 'Debe de escanear un codigo valido',
+                    'is_unique' => 'El correo ya ha sido usado'
+                ]
+            ],
+            ])) {
+                return
+                    view('templates/header') .
+                    view('scanner', array_merge($data, [
+                        'validation' => $this->validator,
+                    ])) .
+                    view('templates/footer');
+            }
+
+            try {
+                $this->visitor->insert([
+                    'email' => $this->request->getPost('email')
+                ]);
+
+                return redirect()->route('/');
+    
+            } catch (\Exception $e) {
+                session()->setFlashdata('error', $e->getMessage());
+                return
+                    view('templates/header') .
+                    view('scanner', array_merge($data, ['error' => $e->getMessage()])) .
+                    view('templates/footer');
+            }
+        
     }
 }
---
+++ app/Views/templates/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="<?= base_url() . "/js/adapter.min.js"?>"></script>
+    <script src="<?= base_url() . "/js/vue.min.js"?>"></script>
+    <script src="<?= base_url() . "/js/instascan.min.js"?>"></script>
+    <!--<script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>-->
+    
+    <!--Bootstrap-->
+    <link rel="stylesheet" href="<?= base_url() . "/bootstrap/css/bootstrap.min.css"?>">
+    <script src="<?= base_url() . "/bootstrap/js/bootstrap.min.js"?>"></script>
+    <script src="<?= base_url() . "/bootstrap/js/popper.min.js"?>"></script>
+
+    <style>
+      body{
+        background: #FBD3E9;
+        background: -webkit-linear-gradient(to left, #FBD3E9, #BB377D);
+        background: linear-gradient(to left, #FBD3E9, #BB377D);
+      }
+    </style>
+
+    <title>TESVG Semana de la Ciencia y la Tecnología</title>
+  </head>
+
+  <body>    
+    <div class="container text-center">
